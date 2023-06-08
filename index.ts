033f2571a0ae5666f391e3ece505b606cf6edecf
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-06-08 21:39:22 +0530

    add typeOrm config

--- index.ts
+++ index.ts
@@ -9,18 +9,26 @@ import { DataSource } from 'typeorm';
 const app: Express = express();
 dotenv.config();
 
+const port = process.env.PORT;
+
 // Create connection
 export const AppDataSource = new DataSource({
   type: 'mysql',
   host: 'localhost',
   port: 3306,
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true,
 });
 
 app.get('/', (req: Request, res: Response) => {
   res.send('<h1>Hello World</h1>');
 });
 
-const Port = process.env.PORT;
-app.listen(Port, () => {
-  console.log('Listening on port: ' + Port);
-});
+AppDataSource.initialize().then(() => {
+  app.listen(port);
+  console.log('Data source has been initialized!');
+}).catch(err => console.log('Error during data source initialization',err));
+
+
