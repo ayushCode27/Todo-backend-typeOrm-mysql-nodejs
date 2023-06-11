3a3fb7c81542dc461130ac7cb979d305000e3000
Ayush <ayushagarwal.dev@gmail.com>
2023-06-11 22:56:16 +0530

    add cors and body-parser middleware

--- index.ts
+++ index.ts
@@ -5,20 +5,25 @@ import express, {
 } from 'express';
 import dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app: Express = express();
 dotenv.config();
 
 const port = process.env.PORT;
 
+app.use(bodyParser.json());
+app.use(cors());
+
 // Create connection
 export const AppDataSource = new DataSource({
   type: 'mysql',
   host: 'localhost',
   port: 3306,
-  username: process.env.MYSQL_USERNAME,
+  username: 'root',
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
+  database: 'todo',
   synchronize: true,
 });
 
@@ -26,9 +31,14 @@ app.get('/', (req: Request, res: Response) => {
   res.send('<h1>Hello World</h1>');
 });
 
-AppDataSource.initialize().then(() => {
-  app.listen(port);
-  console.log('Data source has been initialized!');
-}).catch(err => console.log('Error during data source initialization',err));
-
-
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(port);
+    console.log('Data source has been initialized!');
+  })
+  .catch((err) =>
+    console.log(
+      'Error during data source initialization',
+      err,
+    ),
+  );
