2ad995114f9ec405a61fb83c62f435eb1d615fcb
Ayush <ayushagarwal.dev@gmail.com>
2023-06-15 23:06:51 +0530

    add handlers to controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,27 +1,45 @@
+import { validationResult } from 'express-validator';
 import { AppDataSource } from '../..';
 import { Task } from './tasks.entity';
 import { instanceToPlain } from 'class-transformer';
+import { Request, Response } from 'express';
 
-export class TaskController {
-  constructor(
-    private taskRepository = AppDataSource.getRepository(
-      Task,
-    ),
-  ) {}
-
-  //@ts-ignore
-  public async getAll(): Promise<Task[]> {
+class TaskController {
+  public async getAll(
+    req: Request,
+    res: Response,
+  ): Promise<Response> {
     let allTasks: Task[];
 
     try {
-      allTasks = await this.taskRepository.find({
+      allTasks = await AppDataSource.getRepository(
+        Task,
+      ).find({
         order: { date: 'ASC' },
       });
 
       allTasks = instanceToPlain(allTasks) as Task[];
-      return allTasks;
-    } catch (error) {
-      console.log(error);
+      return res.json(allTasks).status(200);
+    } catch (_error) {
+      return res
+        .json({ error: 'Internal server error' })
+        .status(500);
     }
   }
+
+  public async create(
+    req: Request,
+    res: Response,
+  ): Promise<Response> {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res
+        .json({ errors: errors.array() })
+        .status(400);
+    }
+    return res.json();
+  }
 }
+
+export const taskController = new TaskController();
--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -1,31 +1,13 @@
-import { Router, Request, Response } from 'express';
-import { TaskController } from './tasks.controller';
+import { Router } from 'express';
+import { taskController } from './tasks.controller';
 import { createValidator } from './task.validator';
-import { validationResult } from 'express-validator';
 
 export const taskskRouter: Router = Router();
 
-taskskRouter.get(
-  '/',
-  async (req: Request, res: Response) => {
-    const taskController = new TaskController();
-    const allTasks = await taskController.getAll();
-    res.json(allTasks).status(200);
-  },
-);
+taskskRouter.get('/', taskController.getAll);
 
 taskskRouter.post(
   '/',
   createValidator,
-  //@ts-ignore
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res
-        .json({ errors: errors.array() })
-        .status(400);
-    }
-    // const taskController = new TaskController();
-  },
+  taskController.create,
 );
